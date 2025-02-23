94898b90e76220e7ea68f040bcb37b1924055d53
Lochan-sharma232 <lochansharma537@gmail.com>
2025-02-23 15:06:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const byteSize = (str) => {
 	byteSize='';
-	console.log(byteSize);
+	console.log(TextEncoder().encode(str).length);
   // write your code here
 };
 
 // Do not change the code below
-//  const str = prompt("Enter some string.");
+//   const str = prompt("Enter some string.");
 // alert(byteSize(str));
+//return new TextEncoder().encode(str).length;
